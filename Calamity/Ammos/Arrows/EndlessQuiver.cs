f9d212fb3508be3f7efbcb98079d0e966b0c0d3c
vodavodochka <msadunov@gmail.com>
2024-04-12 09:27:35 +0500

    Endless Quivers

---
+++ Calamity/Ammos/Arrows/EndlessQuiver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using CalamityMod;
+using CalamityMod.Items.Armor.Aerospec;
+using CalamityMod.Items.Accessories;
+using CalamityMod.Items.Weapons.Ranged;
+using CalamityMod.Items.Weapons.Melee;
+using CalamityMod.Items.Pets;
+using CalamityMod.Buffs.Pets;
+using CalamityMod.Projectiles.Pets;
+
+namespace FargoCalamity.Calamity.Ammos.Arrows
+{
+    public class IcecleArrow : BaseAmmo
+    {
+        public override int AmmunitionID => ModLoader.GetMod("CalamityMod").Find<ModItem>("IcecleArrow").Type;
+    }
+    public class ArcticArrow : BaseAmmo
+    {
+        public override int AmmunitionID => ModLoader.GetMod("CalamityMod").Find<ModItem>("ArcticArrow").Type;
+    }
+    public class BloodfireArrow : BaseAmmo
+    {
+        public override int AmmunitionID => ModLoader.GetMod("CalamityMod").Find<ModItem>("BloodfireArrow").Type;
+    }
+    public class VanquisherArrow : BaseAmmo
+    {
+        public override int AmmunitionID => ModLoader.GetMod("CalamityMod").Find<ModItem>("VanquisherArrow").Type;
+    }
+    public class TerraArrow : BaseAmmo
+    {
+        public override int AmmunitionID => ModLoader.GetMod("CalamityMod").Find<ModItem>("TerraArrow").Type;
+    }
+    public class ElysianArrow : BaseAmmo
+    {
+        public override int AmmunitionID => ModLoader.GetMod("CalamityMod").Find<ModItem>("ElysianArrow").Type;
+    }
+    public class NapalmArrow : BaseAmmo
+    {
+        public override int AmmunitionID => ModLoader.GetMod("CalamityMod").Find<ModItem>("NapalmArrow").Type;
+    }
+}
---
+++ Calamity/Ammos/BaseAmmo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using CalamityMod;
+using CalamityMod.Items.Armor.Aerospec;
+using CalamityMod.Items.Accessories;
+using CalamityMod.Items.Weapons.Ranged;
+using CalamityMod.Items.Weapons.Melee;
+using CalamityMod.Items.Pets;
+using CalamityMod.Buffs.Pets;
+using CalamityMod.Projectiles.Pets;
+
+
+namespace FargoCalamity.Calamity
+{
+    public abstract class BaseAmmo : ModItem
+    {
+        public abstract int AmmunitionID { get; }
+
+
+        public override void SetStaticDefaults()
+        {
+
+        }
+
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(AmmunitionID);
+            Item.width = 26;
+            Item.height = 26;
+            Item.consumable = false;
+            Item.maxStack = 1;
+            Item.value *= 3996;
+            Item.rare += 1;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(AmmunitionID, 3996)
+                .AddTile(ModLoader.GetMod("CalamityMod").Find<ModTile>("CosmicAnvilItem").Type)
+                .Register();
+        }
+    }
+}
